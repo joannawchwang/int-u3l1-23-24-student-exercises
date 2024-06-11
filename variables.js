3f7fc5da8e6646f15d8b4a31edf62d7808436831
joannawchwang <joannawch0301@gmail.com>
2024-06-11 10:59:26 -0500

    3.1 lesson
    Variable setting up

--- variables.js
+++ variables.js
@@ -1,18 +1,23 @@
 // CODE ALONG, PART 1
-let favoriteColor;
+let favoriteColor = 'White';
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood = 'mango';
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber = 6;
 
 
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
 // - Use a string value of your favorite color.
+// favoriteColor = 'white';
 // - Use a string value of your favorite food.
+// favoriteFood = 'mango';
 // - Use a number value of your favorite number.
-
+// favoriteNumber = 6;
 // 4. Console log each variable on a new line.
+console.log(favoriteColor);
+console.log(favoriteNumber);
+console.log(favoriteFood);
